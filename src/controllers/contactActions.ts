c4812e982e8eb708afdb9accc6b4ff51e5e68c11
Tamjid Ahmed <tamjidahammad1996@gmail.com>
2023-04-28 19:18:28 +0600

    added new features

--- src/controllers/contactActions.ts
+++ src/controllers/contactActions.ts
@@ -1,23 +1,25 @@
 import { Request,Response } from "express";
-export const contactGet= (req:Request, res:Response)=>{
+import asyncHandler from "express-async-handler";
+export const contactGet= asyncHandler (async (req:Request, res:Response)=>{
     res.status(200).json({message:"Got all the message"})
-}
-export const contactGetById= (req:Request, res:Response)=>{
-    res.status(200).json({message:`Got the message for ${req.params.id} id`})
-}
+})
 
-export const createNew= (req:Request, res:Response)=>{
-    // console.log(req.body)
+export const contactGetById= asyncHandler (async (req:Request, res:Response)=>{
+    res.status(200).json({message:`Got the message for ${req.params.id} id`})
+})
+
+export const createNew= asyncHandler (async (req:Request, res:Response)=>{
     if (!req.body.name || !req.body.age || !req.body.phone_number){
-        // console.log("error")
         res.status(400)
         throw new Error("All fields are required")
     }
     res.status(201).json({message:"Created the message"})
 }
-export const updateById = (req:Request,res:Response) =>{
+)
+export const updateById = asyncHandler( async (req:Request,res:Response) =>{
     res.status(200).json({message:`Updated the message for ${req.params.id}`})
-}
-export const deleteById = (req:Request,res:Response) =>{
+})
+
+export const deleteById = asyncHandler (async (req:Request,res:Response) =>{
     res.status(200).json({message:`Deleted the message for ${req.params.id}`})
-}
+})
