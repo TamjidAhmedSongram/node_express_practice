ed815269857a25763addf08ef5c647f3968e424a
Tamjid Ahmed <tamjidahammad1996@gmail.com>
2023-05-07 07:24:23 +0600

    added new features

--- src/controllers/userActions.ts
+++ src/controllers/userActions.ts
@@ -1,20 +1,38 @@
 import { Request, Response } from "express";
-import  asyncHandler  from "express-async-handler";
+import asyncHandler from "express-async-handler";
+import userModel from "../models/userModels";
+import { error } from "console";
 
-export const loginActions = asyncHandler(async (req:Request,res:Response) => {
+export const loginActions = asyncHandler(
+  async (req: Request, res: Response) => {
     res.status(200).json({
-        message:"Logged in"
-    })
-})
+      message: "Logged in",
+    });
+  }
+);
 
-export const registerActions = asyncHandler(async (req:Request,res:Response) => {
-    res.status(200).json({
-        message:"Registered a user"
-    })
-})
+export const registerActions = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email, password, userName } = req.body;
+    // console.log(email, password, userName)
+    const existingUser = await userModel.findOne({ $or: [email, userName] });
 
-export const currentActions = asyncHandler(async (req:Request,res:Response) => {
+    if (existingUser) {
+      res.status(400);
+      throw new Error("User already Register same name or email");
+    }
+
+
     res.status(200).json({
-        message:"Current user"
-    })
-})
+      message: "Registered a user",
+    });
+  }
+);
+
+export const currentActions = asyncHandler(
+  async (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "Current user",
+    });
+  }
+);
--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,2 +1,21 @@
 import { Schema , model } from "mongoose";
 
+const userSchema = new Schema({
+    userName:{
+        type:String,
+        required:[true,"This username field is required"],
+        unique:[true,"This name is aleready taken"]
+    },
+    password:{
+        type:String,
+        required:[true,"This password field cant be empty"],
+    },
+    email:{
+        type:String,
+        required:[true,"This email is required"],
+        unique:[true,"This email is already taken"]
+    }},{timestamps:true})
+
+    const userModel = model("userSchema",userSchema)
+
+    export default userModel
